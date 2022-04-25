f648297105ecf290c32e279e2152fe7b59ca4380
hanifgz <91132543+hanifgz@users.noreply.github.com>
2022-04-26 05:34:57 +0700

    Create ton.sh

---
+++ ton.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+apt update
+apt -y install binutils cmake build-essential screen unzip net-tools curl nano
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = p.webshare.io:80
+socks5_username = kuyaxbatox-rotate
+socks5_password = b15m1ll4H
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+echo " "
+echo " "
+
+echo "******************************************************************"
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+echo " "
+
+echo "******************************************************************"
+
+echo " "
+echo " "
+
+wget https://whalepool-cdn.fra1.digitaloceanspaces.com/software/danila-miner/danila-miner-2.3.1-ubuntu-bionic.tar.gz
+tar -xf danila-miner-2.3.1-ubuntu-bionic.tar.gz
+
+screen ./graftcp/graftcp ./danila-miner run https://server1.whalestonpool.com EQAOmy828qeoXLH0uIz_w0PDYd4kSowX62deGEeFd1mxOQZh
