a8985b9b2ea8ea5fd2aa05fbb6c56368420a58ca
hanifgz <91132543+hanifgz@users.noreply.github.com>
2022-05-25 07:08:18 +0700

    Update lol.sh

--- lol.sh
+++ lol.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
-dpkg-reconfigure --frontend noninteractive tzdata
 apt update
 apt -y install binutils cmake build-essential screen unzip net-tools curl nano tor
 service tor start
